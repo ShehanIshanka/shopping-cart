6afb727e577ed5ff789d0f41671114a735345dc8
Shehan Ishanka <shehan.ishanka@h2o.ai>
2023-10-09 00:32:51 +0530

    Add swagger

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from src.bootstrap import bootstrap_database, initialize_services
-from src.routers.v1 import cart, product
-
+from src.routers.v1 import cart, product, swagger
 
 app: Flask = Flask(__name__)
 app.register_blueprint(product.products)
 app.register_blueprint(cart.carts)
+app.register_blueprint(swagger.swagger_ui)
 
 db = bootstrap_database()
 initialize_services(db, app)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
